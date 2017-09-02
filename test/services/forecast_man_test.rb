587f37b9fa9cf92ec8827dd620e9b65d08f68496
Mihai <mihai.iftode94@gmail.com>
2017-09-03 01:27:32 +0200

    [#3] Update forecast_man tests

--- test/services/forecast_man_test.rb
+++ test/services/forecast_man_test.rb
@@ -11,31 +11,31 @@ describe ForecastMan do
 
     it "returns temperature forecast data for the next day for the id" do
       result = nil
-      VCR.use_cassette("OpenWeather - temperature forecast", record: :none) do
+      VCR.use_cassette("OpenWeather - one location ten days") do
         result = subject
       end
 
       assert_equal "Sibiu", result["city"]["name"]
       assert_equal 10, result["cnt"]
-      assert_equal 14, result["list"][0]["temp"]["day"]
-      assert_equal 17.42, result["list"][9]["temp"]["day"]
+      assert_equal 23, result["list"][0]["temp"]["day"]
+      assert_equal 21.54, result["list"][9]["temp"]["day"]
     end
   end
 
-  describe "next_day_temperatures" do
-    let(:ids) { [2_618_425, 2_950_096, 667_268] }
+  describe "next_day_temperature" do
+    let(:id) { 2_618_425 }
 
-    subject { instance.next_day_temperatures(ids) }
+    subject { instance.next_day_temperature(id) }
 
     it "returns temperature for the next day of the selected id" do
       result = nil
-      VCR.use_cassette("OpenWeather - temperature next weather", record: :none) do
+      VCR.use_cassette("OpenWeather - multiple locations one day") do
         result = subject
       end
 
-      assert_equal "Copenhagen", result[0]["city"]["name"]
-      assert_equal 1, result[0]["cnt"]
-      assert_equal 13.61, result[0]["list"][0]["temp"]["day"]
+      assert_equal "Copenhagen", result["city"]["name"]
+      assert_equal 1, result["cnt"]
+      assert_equal 9.58, result["list"][0]["temp"]["day"]
     end
   end
 end
